7de73cd065d23bd0f8a908d78097e1f619af4532
Avisha Dilhara <avishadilhara.edu@gmail.com>
2024-03-22 07:54:42 +0530

    Refactor AdvertPage and ProductIntroduction components

--- components/productIntorduction.tsx
+++ components/productIntorduction.tsx
@@ -13,32 +13,34 @@ export default function Component() {
   return (
     <ScrollIntoView>
     <section  className="w-full py-12 px-4 my-36 mt-44">
-      
 
-            <div className="">
 
-              <div className="lg:flex lg:flex-auto lg:flex-row">
-                <div className="flex flex-auto flex-row items-center mr-36">
-                  <Image
-                    src={Phone}
-                    alt="Phone Image"
-                    className=" w-3/4" // Adjust the width and height as needed
-                  />
-                  <div className="lg:mt-4 my-3  text-center">
-                       <h1 className=" text-2xl font-bold ">Add your Study Materials</h1>
-                       <p className=" mt-5 "> Discover a wide range of study materials meticulously crafted to help you deepen your understanding
-                         and master key concepts. Whether your preparing for exams, expanding your knowledge,
-                          or simply exploring new subjects, our resources are tailored to your needs.</p>
+            <div className=" ">
+                
+                <div className="lg:flex lg:flex-auto">
+                  <div className="flex flex-col lg:flex-row items-center lg:mr-36 justify-center">
+                    <Image
+                      src={Phone}
+                      alt="Phone Image"
+                      className=" lg:w-3/4 w-screen" // Adjust the width and height as needed
+                    />
+                    <div className="lg:mt-4 my-3  text-center">
+                        <h1 className="text-4xl font-bold ">Add your Study Materials</h1>
+                        <p className=" mt-5 "> Discover a wide range of study materials meticulously crafted to help you deepen your understanding
+                          and master key concepts. Whether your preparing for exams, expanding your knowledge,
+                            or simply exploring new subjects, our resources are tailored to your needs.</p>
+                    </div>
+                      
                   </div>
-                     
+
                 </div>
-              </div>
+           
               
-              <div className="lg:flex lg:flex-auto lg:flex-row mt-36">
-                <div className="flex flex-auto flex-row justify-center lg:mt-36">
+              <div className="lg:flex lg:flex-auto  mt-36">
+                <div className="flex flex-col-reverse lg:flex-row  justify-center mt-36">
                  
                  <div className=" my-3 text-center lg:ml-10">
-                    <h1 className="text-2xl font-bold">Generate Custom Quizzes</h1>
+                    <h1 className="text-4xl font-bold">Generate Custom Quizzes</h1>
                     <p className=" mt-5">Create personalized quizzes with just a few clicks based on the content 
                         your been studying. Whether you want to test your own knowledge or 
                         challenge your friends, our quizzes are a fun and effective way to reinforce 
@@ -56,15 +58,15 @@ export default function Component() {
 
               </div>
 
-              <div className="lg:flex lg:flex-auto lg:flex-row lg:mt-36">
-                <div className="flex flex-auto flex-row items-center">
+              <div className="lg:flex lg:flex-auto mt-36">
+                <div className="flex flex-col lg:flex-row  items-center">
                   <Image
                     src={Phonelaptop}
                     alt="Phone Image"
                     className=" w-3/5" // Adjust the width and height as needed
                   />
                   <div className=" my-3 text-center lg:ml-10">
-                    <h1 className="text-2xl font-bold">Engage with Quizzifyme Chat</h1>
+                    <h1 className="text-4xl font-bold">Engage with Quizzifyme Chat</h1>
                     <p className=" mt-5">Interact with Quizzifyme Chat, an interactive feature designed to enhance your learning experience. 
                         Ask questions, seek clarification, or engage in discussions related to the study materials and quizzes. 
                         Quizzifyme Chat provides you with personalized support and guidance, making your learning journey more engaging 
